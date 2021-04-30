75b5e0fa33224be84406cb3106201aa6bf989d98
kleitonalbuquerque <kleiton2102@gmail.com>
2021-04-30 15:43:44 -0300

    Repetição

---
+++ src/data/produtos.js
@@ -0,0 +1,12 @@
+export default [
+  // id, nome, preco
+  { id: 1, nome: "Sabão em Pó", preco: 13.9 },
+  { id: 2, nome: "Arroz", preco: 23.0 },
+  { id: 3, nome: "Feijão", preco: 6.9 },
+  { id: 4, nome: "Manteiga", preco: 23.0 },
+  { id: 5, nome: "Papinha", preco: 3.9 },
+  { id: 6, nome: "Achocolatado", preco: 8.9 },
+  { id: 7, nome: "Leite", preco: 3.79 },
+  { id: 8, nome: "Macarrão", preco: 4.0 },
+  { id: 9, nome: "Filé de Frango", preco: 9.9 },
+];
