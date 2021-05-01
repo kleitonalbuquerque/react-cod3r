117e9664d69a3cd633a0872b3069f87d8cf30715
kleitonalbuquerque <kleiton2102@gmail.com>
2021-05-01 19:30:39 -0300

    [refact]Contador #02

--- src/components/contador/Contador.jsx
+++ src/components/contador/Contador.jsx
@@ -1,6 +1,10 @@
 import "./Contador.css";
 import React, { Component } from "react";
 
+import Display from "./Display";
+import Botoes from "./Botoes";
+import PassoForm from "./PassoForm";
+
 class Contador extends Component {
   // Sem construtor
   state = {
@@ -20,37 +24,20 @@ class Contador extends Component {
     });
   };
 
-  setPasso = (e) => {
+  setPasso = (novoPasso) => {
     this.setState({
-      passo: +e.target.value,
+      passo: novoPasso,
+      // passo: +e.target.value,
     });
   };
-
-  // Com construtor
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     numero: props.numeroInicial,
-  //   };
-  // }
 
   render() {
     return (
       <div className="Contador">
         <h2>Contador</h2>
-        <h3>{this.state.numero}</h3>
-        <div>
-          <label htmlFor="passoInput">Passo: </label>
-          <input
-            id="passoInput"
-            type="number"
-            value={this.state.passo}
-            onChange={this.setPasso}
-          />
-        </div>
-        <button onClick={this.inc}>+</button>
-        <button onClick={this.dec}>-</button>
+        <Display numero={this.state.numero} />
+        <PassoForm passo={this.state.passo} setPasso={this.setPasso} />
+        <Botoes setInc={this.inc} setDec={this.dec} />
       </div>
     );
   }
