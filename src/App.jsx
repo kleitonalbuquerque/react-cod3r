9dfb0dd229ddfee6ac27021aff79435214a4cd08
kleitonalbuquerque <kleiton2102@gmail.com>
2021-05-02 23:30:47 -0300

    Desafio Mega Sena

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import Card from "./components/layout/Card";
 
+import Mega from "./components/mega/Mega";
 import Contador from "./components/contador/Contador";
 import Input from "./components/formulario/Input";
 import IndiretaPai from "./components/comunicacao/IndiretaPai";
@@ -25,6 +26,10 @@ export default () => (
     <div className="Cards">
       <Card titulo={tag}>
         <strong>Olá React!</strong>
+      </Card>
+
+      <Card titulo="#13 - Mega" color="#B9006E">
+        <Mega qtde={6} />
       </Card>
 
       <Card titulo="#12 - Contador" color="#424242">
